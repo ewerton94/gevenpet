b1d2fe84a52794c22043560af442f7b628a01a9e
Ewerton Oliveira <ewerton.amorim@live.com>
2017-10-14 16:04:06 -0230

    Criado views para página inicial

--- evento/views.py
+++ evento/views.py
@@ -1,3 +1,8 @@
+'''This is the views'''
 from django.shortcuts import render
+from .models import Sobre
 
-# Create your views here.
+def inicio(request):
+    '''This is the view of homepage'''
+    contexto = Sobre.objects.all()
+    return render(request, 'inicio.html', contexto)
